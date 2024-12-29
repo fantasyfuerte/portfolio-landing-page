ec25983b56cc1dea34944817e4f3f332f367c450
fuerte <leandrofuerte75@gmail.com>
2024-12-29 18:29:41 +0100

    Deleting unused styles in the global CSS file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-6xl font-semibold">Welcome to my portfolio</h1>
+      <h1 className="text-6xl font-semibold">Welcome to my portfolio page</h1>
     </main>
   );
 }
