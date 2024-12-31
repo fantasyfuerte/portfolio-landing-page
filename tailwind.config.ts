210817e0925d0a001a99250a95a2b199a71d0060
fuerte <leandrofuerte75@gmail.com>
2024-12-31 20:16:17 +0100

    Update Tailwind CSS configuration with new color and background image settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: "#f5741c",
+        darkBg: "#131424",
+      },
+      backgroundImage: {
+        'gradient-cover': "linear-gradient(90.21deg, rgba(70,54,24,0.5) -5.91%, rgba(74,47,189,0.5) 111.58%)",
       },
     },
   },
   plugins: [],
 } satisfies Config;
+
+export default config;
