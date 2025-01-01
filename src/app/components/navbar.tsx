77f9fcd4a9a1a38c8865af50a4a29022f64d6786
fuerte <leandrofuerte75@gmail.com>
2025-01-01 18:56:51 +0100

    Styling navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,11 @@
 const Navbar = () => {
   return (
     <div className="fixed flex flex-col items-center justify-center w-full mt-auto h-max bottom-10">
-      This text is in the navbar
+      <nav>
+        <div className="flex justify-center items-center w-full gap-2 px-4 py-1 bg-white/15 rounded-full backdrop-blur-sm">
+          This text is from the nav
+        </div>
+      </nav>
     </div>
   );
 };
