efb6479225e87fa2ed4288ef8bca7bee8aafb860
fuerte <leandrofuerte75@gmail.com>
2025-01-01 10:43:44 +0100

    Fixing tsparticles bug with 0px width div

--- src/app/components/cover-particles.tsx
+++ src/app/components/cover-particles.tsx
@@ -27,71 +27,73 @@ const CoverParticles = () => {
 
   return (
     init && (
-      <Particles
-        id="tsparticles"
-        options={{
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
+      <div className="w-0">
+        <Particles
+          id="tsparticles"
+          options={{
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+              modes: {
+                push: {
+                  quantity: 1,
+                },
+                repulse: {
+                  distance: 100,
+                  duration: 0.5,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
                 enable: true,
-                mode: "push",
+                opacity: 0.5,
+                width: 1,
               },
-              onHover: {
+              move: {
+                direction: "none",
                 enable: true,
-                mode: "repulse",
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 1,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
               },
             },
-            modes: {
-              push: {
-                quantity: 1,
-              },
-              repulse: {
-                distance: 100,
-                duration: 0.5,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 1,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
+            detectRetina: true,
+          }}
+        />
+      </div>
     )
   );
 };
