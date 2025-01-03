57e12d253e27fc2b5579d923487e56b55b6afae3
fuerte <leandrofuerte75@gmail.com>
2025-01-03 18:01:01 +0100

    Creating FadeIn Animation

--- utils/motion-transitions.tsx
+++ utils/motion-transitions.tsx
@@ -12,3 +12,30 @@ export const transitionVariantsPage = {
     width: ["0%", "100%"],
   },
 };
+
+export const fadeIn = (position: string) => {
+  return {
+    visible: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        duration: 1.4,
+        delay: 0.5,
+        ease: [0.25, 0.25, 0.25, 1],
+      },
+      hidden: {
+        y: position == "bottom" ? -80 : 0,
+        x: position == "right" ? -80 : 0,
+        opacity: 0,
+        transition: {
+          type: "tween",
+          duration: 1.4,
+          delay: 0.5,
+          ease: [0.25, 0.25, 0.25, 1],
+        },
+      },
+    },
+  };
+};
