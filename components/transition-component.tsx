c05b045e64029650bea42c99fb1d8b80ccc35f1e
fuerte <leandrofuerte75@gmail.com>
2025-01-03 18:27:55 +0100

    Refine MotionTransition component to restrict position prop to "right" or "bottom" and add initial/animate states for improved animation control

--- components/transition-component.tsx
+++ components/transition-component.tsx
@@ -5,13 +5,17 @@ import { fadeIn } from "../utils/motion-transitions";
 
 interface Props {
   children: React.ReactNode;
-  position: string;
+  position: "right" | "bottom";
 }
 
 const MotionTransition = (props: Props) => {
   const { children, position } = props;
 
-  return <motion.div variants={fadeIn(position)}>{children}</motion.div>;
+  return (
+    <motion.div variants={fadeIn(position)} initial="hidden" animate="visible">
+      {children}
+    </motion.div>
+  );
 };
 
 export default MotionTransition;
