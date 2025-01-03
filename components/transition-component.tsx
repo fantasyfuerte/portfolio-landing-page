ab3cf0739c62edc7b7195c99a005d8c8eea8f562
fuerte <leandrofuerte75@gmail.com>
2025-01-03 18:25:08 +0100

    Add position prop to MotionTransition and apply fadeIn animation

--- components/transition-component.tsx
+++ components/transition-component.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { motion } from "motion/react";
+import { fadeIn } from "../utils/motion-transitions";
 
 interface Props {
   children: React.ReactNode;
+  position: string;
 }
 
 const MotionTransition = (props: Props) => {
-  const { children } = props;
+  const { children, position } = props;
 
-  return <motion.div>{children}</motion.div>;
+  return <motion.div variants={fadeIn(position)}>{children}</motion.div>;
 };
 
 export default MotionTransition;
