c0d5bce746e18313f3b4a5d473afd20fa570ca93
fuerte <leandrofuerte75@gmail.com>
2025-01-03 17:37:02 +0100

    Styling NavBar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { itemsNavbar } from "../data";
+import { usePathname } from "next/navigation";
+import Link from "next/link"; 
+
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="fixed flex flex-col items-center justify-center w-full mt-auto h-max bottom-10">
       <nav>
         <div className="flex justify-center items-center w-full gap-2 px-4 py-1 bg-white/15 rounded-full backdrop-blur-sm">
-          This text is from the nav
+          {itemsNavbar.map((item) => (
+            <div
+              key={item.id}
+              className={`${
+                pathname == item.link && "bg-secondary"
+              } px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary`}
+            >
+              <Link href={item.link}>{item.icon}</Link>
+            </div>
+          ))}
         </div>
       </nav>
     </div>
@@ -11,4 +28,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
