58660a06d4e1965bd5af82380117a58ccdda236f
fuerte <leandrofuerte75@gmail.com>
2025-01-03 18:31:35 +0100

    Add className prop to MotionTransition for custom styling

--- components/transition-component.tsx
+++ components/transition-component.tsx
@@ -6,13 +6,20 @@ import { fadeIn } from "../utils/motion-transitions";
 interface Props {
   children: React.ReactNode;
   position: "right" | "bottom";
+  className: string;
 }
 
 const MotionTransition = (props: Props) => {
-  const { children, position } = props;
+  const { children, position, className } = props;
 
   return (
-    <motion.div variants={fadeIn(position)} initial="hidden" animate="visible">
+    <motion.div
+      variants={fadeIn(position)}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      className={className}
+    >
       {children}
     </motion.div>
   );
