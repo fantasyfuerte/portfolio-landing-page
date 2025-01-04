8234e0ca8a15eded2d6b184eb9b89f00e11ae3ff
fuerte <leandrofuerte75@gmail.com>
2025-01-04 12:32:12 +0100

    Enhance TransitionPage animation with initial, animate, and exit states

--- src/components/transition-page.tsx
+++ src/components/transition-page.tsx
@@ -10,6 +10,10 @@ function TransitionPage() {
         <motion.div
           className="fixed top-0 bottom-0 right-full w-screen z-30 bg-[#2e2257]"
           variants={transitionVariantsPage}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
         />
       </div>
     </AnimatePresence>
