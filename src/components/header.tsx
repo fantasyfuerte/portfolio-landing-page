a1f3ce90e4cca703a9fb8d127063acb5a8ffce4b
fuerte <leandrofuerte75@gmail.com>
2025-01-04 12:47:01 +0100

    Wrap Header component in MotionTransition for enhanced animation effects

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,16 @@
+import MotionTransition from "./transition-component";
+
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <MotionTransition
+      position="bottom"
+      className="absolute z-40 inline-block w-full top-5 md:top-10"
+    >
+      <header>
+        <div className="container justify-between max-w-6xl mx-auto md:flex"></div>
+      </header>
+    </MotionTransition>
+  );
 }
 
-export default Header
+export default Header;
