e0c10a7a5f61b7f4f81c0129cfe5c5213ad570de
fuerte <leandrofuerte75@gmail.com>
2025-01-04 12:41:48 +0100

    Add Header component to RootLayout for improved structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Urbanist } from "next/font/google";
 import Navbar from "@/components/navbar";
+import Header from "@/components/header";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={urbanist.className}>
+        <Header />
         <Navbar />
         {children}
       </body>
