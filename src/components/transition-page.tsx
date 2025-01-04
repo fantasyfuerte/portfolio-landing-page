fa73e9afbf73169d39fa88f9be48ddae44b380fd
fuerte <leandrofuerte75@gmail.com>
2025-01-04 12:29:58 +0100

    Implement TransitionPage component with animation using AnimatePresence and motion

--- src/components/transition-page.tsx
+++ src/components/transition-page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { transitionVariantsPage } from "@/utils/motion-transitions";
+import { AnimatePresence, motion } from "motion/react";
+
 function TransitionPage() {
-  return <div>TransitionPage</div>;
+  return (
+    <AnimatePresence mode="wait">
+      <div>
+        <motion.div
+          className="fixed top-0 bottom-0 right-full w-screen z-30 bg-[#2e2257]"
+          variants={transitionVariantsPage}
+        />
+      </div>
+    </AnimatePresence>
+  );
 }
 
 export default TransitionPage;
