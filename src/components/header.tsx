9e8cd488fe9c5b74d416cf69dc401cb73d5d3ce9
fuerte <leandrofuerte75@gmail.com>
2025-01-06 19:39:23 +0100

    Adding socialNetworks

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import MotionTransition from "./transition-component";
+import { socialNetworks } from "@/data";
 
 function Header() {
   return (
@@ -12,11 +13,16 @@ function Header() {
           <Link href="/">
             <h1 className="my-3 text-4xl font-bold text-center md:text-left">
               Fuerte
-              <span className="text-secondary">
-                Dev
-              </span>
+              <span className="text-secondary">Dev</span>
             </h1>
           </Link>
+            <div className="flex items-center justify-center gap-7">
+              {socialNetworks.map(({ logo, src, id }) => (
+                <Link key={id} href={src} target="_blank">
+                  {logo}
+                </Link>
+              ))}
+            </div>
         </div>
       </div>
     </MotionTransition>
