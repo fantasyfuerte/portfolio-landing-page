8de51668d2218c7ddd10ff4542f0e8d32a5b4482
fuerte <leandrofuerte75@gmail.com>
2025-01-07 09:04:01 +0100

    Enhance Introduction component with image and styling; update Navbar z-index

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
 function Introduction() {
-  return <div>Introduction</div>;
+  return (
+    <div className="z-20 w-full bg-darkBg/60">
+      <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
+        <Image src="/home-4.png" alt="principal-image" priority width="800" height="800"></Image>
+      </div>
+    </div>
+  );
 }
 
 export default Introduction;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <MotionTransition
       position="right"
-      className="fixed flex flex-col items-center justify-center w-full mt-auto h-max bottom-10"
+      className="z-40 fixed flex flex-col items-center justify-center w-full mt-auto h-max bottom-10"
     >
       <nav>
         <div className="flex justify-center items-center w-full gap-2 px-4 py-1 bg-white/15 rounded-full backdrop-blur-sm">
