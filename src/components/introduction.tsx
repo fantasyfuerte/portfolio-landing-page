e941d94965bff7cf2ade1f508cb24a9e8fea67d7
fuerte <leandrofuerte75@gmail.com>
2025-01-07 09:52:14 +0100

    Styling introduction title

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -5,7 +5,16 @@ function Introduction() {
   return (
     <div className="z-20 w-full bg-darkBg/60">
       <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
-        <Image src="/home-4.png" alt="principal-image" priority width="800" height="800"></Image>
+        <Image
+          src="/home-4.png"
+          alt="principal-image"
+          priority
+          width="800"
+          height="800"
+        ></Image>
+        <div className="flex flex-col justify-center">
+            <h1 className="mb-5 text-2xl leading-tight text-center md:text-left">Si puedes pensarlo</h1>
+        </div>
       </div>
     </div>
   );
