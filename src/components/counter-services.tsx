4d7878fe58ef6e45e5165d87ea79902709ddf897
fuerte <leandrofuerte75@gmail.com>
2025-01-09 20:32:29 +0100

    Styling

--- src/components/counter-services.tsx
+++ src/components/counter-services.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { dataCounter } from "@/data";
+import Counter from "react-countup";
+
 function CounterServices() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-center md:text-left">
-        Servicios de <span className="text-secondary">contabilidad</span>
-      </h1>
+    <div className="grid justify-between max-w-3xl grid-cols-2 gap-3 mx-auto my-8 md:grid-cols-4 md:gap-6">
+      {dataCounter.map(
+        ({ id, endCounter, text, lineRight, lineRightMobile }) => (
+          <div key={id} className={`${lineRight && "ltr"}`}>
+            <div className={`${lineRightMobile && "px-4 border-2 border-transparent md:border-e-gray-100"}`}>
+              <p>
+                + <Counter start={0} end={endCounter} duration={5} />
+              </p>
+              <p className="text-xs uppercase max-w-[100px]">{text}</p>
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 }
