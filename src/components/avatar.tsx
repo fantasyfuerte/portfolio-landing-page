9890b9e7743f66088a2ad906e4036df4bb12e126
fuerte <leandrofuerte75@gmail.com>
2025-01-09 14:11:00 +0100

    Refactoring img

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import MotionTransition from './transition-component'
-import Image from 'next/image'
+import React from "react";
+import MotionTransition from "./transition-component";
+import Image from "next/image";
 
 function AvatarComponent() {
   return (
-    <MotionTransition position='bottom' className='bottom-0 right-0 hidden md:inline-block md:absolute'>
-        <Image className='w-full h-full' src="/avatar-1.png" alt="avatar" width={400} height={400} />
+    <MotionTransition
+      position="bottom"
+      className="bottom-0 right-0 hidden md:inline-block md:absolute "
+    >
+      <Image
+        src="/avatar-1.png"
+        width="400"
+        height="400"
+        className="w-full h-full "
+        alt="About me"
+      />
     </MotionTransition>
-  )
+  );
 }
 
-export default AvatarComponent
+export default AvatarComponent;
