0dc16a585da7b25694ec5acb3d59f7977a8bd463
fuerte <leandrofuerte75@gmail.com>
2025-01-12 17:25:29 +0100

    Adding time to timeline

--- src/components/time-line.tsx
+++ src/components/time-line.tsx
@@ -12,7 +12,9 @@ function TimeLine() {
                 <h3 className="mb-1 text-2xl font-bold sm:mb-0">
                   {data.title}
                 </h3>
-                <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:mñ-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5"></div>
+                <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:mñ-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                  <time>{data.date}</time>
+                </div>
               </div>
             );
           })}
