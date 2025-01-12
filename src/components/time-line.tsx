a7bbd9bee9911859788c2fae09c1b721a4aebe4c
fuerte <leandrofuerte75@gmail.com>
2025-01-12 17:20:21 +0100

    Styling timeline

--- src/components/time-line.tsx
+++ src/components/time-line.tsx
@@ -8,15 +8,11 @@ function TimeLine() {
         <div className="-my-6">
           {dataAboutPage.map((data) => {
             return (
-              <div key={data.id} className="relative py-6 pl-8 sm:pl-3">
-                <div className="flex flex-col items-center justify-center">
-                  <div className="text-center text-3xl font-bold text-slate-900">
-                    {data.title}
-                  </div>
-                  <div className="text-center text-lg font-medium text-slate-500">
-                    {data.description}
-                  </div>
-                </div>
+              <div key={data.id} className="relative py-6 pl-8 sm:pl-32 group">
+                <h3 className="mb-1 text-2xl font-bold sm:mb-0">
+                  {data.title}
+                </h3>
+                <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:mñ-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5"></div>
               </div>
             );
           })}
