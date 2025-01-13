971f2aee28eb030dce9f90b1340dc329a8752811
fuerte <leandrofuerte75@gmail.com>
2025-01-13 11:27:25 +0100

    Styling title

--- src/app/(routes)/services/page.tsx
+++ src/app/(routes)/services/page.tsx
@@ -11,7 +11,7 @@ function ServicesPage() {
       <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
         <div className="max-w-[450px]">
           <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
-            Mis servicios
+            Mis <span className="text-secondary font-bold">servicios</span>
           </h1>
         </div>
       </div>
