794b8b32d69814a6f4880daecddfdc60ccb40028
fuerte <leandrofuerte75@gmail.com>
2025-01-13 11:43:10 +0100

    Writting services paragraph

--- src/app/(routes)/services/page.tsx
+++ src/app/(routes)/services/page.tsx
@@ -8,11 +8,19 @@ function ServicesPage() {
       <TransitionPage />
       <CircleImage />
       <AvatarServices />
-      <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
+      <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2 md:px-20">
         <div className="max-w-[450px]">
           <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
             Mis <span className="text-secondary font-bold">servicios</span>
           </h1>
+          <p className="mb-3 text-xl text-gray-300">
+            Ofrezco servicios de desarrollo web frontend especializados en la
+            creación de sitios web y aplicaciones atractivas y funcionales.
+            Utilizando las últimas tecnologías, como HTML, CSS y JavaScript,
+            diseño interfaces de usuario intuitivas y responsivas que reflejan
+            la identidad de marca de mis clientes y mejoran su presencia en
+            línea.
+          </p>
         </div>
       </div>
     </>
