cb73e3ee2a64a523738e401f9f188056ef42b74e
fuerte <leandrofuerte75@gmail.com>
2025-01-13 09:27:06 +0100

    Finishing CircleImage component

--- src/components/circle-image.tsx
+++ src/components/circle-image.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
+
 function CircleImage() {
   return (
-    <div className="bottom-0 right-0 hidden md:inline-block md:absolute"></div>
+    <div className="bottom-0 right-0 hidden md:inline-block md:absolute">
+      <Image
+        src="./circles.png"
+        width="300"
+        height="300"
+        alt="Circle"
+        className="w-full h-full"
+      />
+    </div>
   );
 }
 
