0e04754d7dee62decab233135ca519dd59b04856
fuerte <leandrofuerte75@gmail.com>
2025-01-13 09:02:25 +0100

    Fixing avatar

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import MotionTransition from "./transition-component";
 import Image from "next/image";
@@ -6,14 +8,13 @@ function AvatarComponent() {
   return (
     <MotionTransition
       position="bottom"
-      className="hidden md:inline-block md:absolute md:bottom-0 md:right-0 
-      w-[450px]"
+      className="hidden md:inline-block md:absolute md:bottom-0 md:right-0"
     >
       <Image
         src="/avatar-1.png"
-        width="400"
-        height="400"
-        className="w-full h-full "
+        width="300"
+        height="300"
+        className="w-full h-full"
         alt="About me"
       />
     </MotionTransition>
