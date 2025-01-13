9156e6774826d06fd2aa615d299b29d142a3c30c
fuerte <leandrofuerte75@gmail.com>
2025-01-13 11:27:03 +0100

    Fixing images

--- src/components/avatar-services.tsx
+++ src/components/avatar-services.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image"
-import MotionTransition from "./transition-component"
+import Image from "next/image";
+import MotionTransition from "./transition-component";
 
 function AvatarServices() {
   return (
-    <MotionTransition position="right" className="bottom-0 left-0 hidden md:inline-block md:absolute">
-        <Image src="/services.png" width={300} height={300} alt="services-avatar" className="w-[350px] h-full" />
+    <MotionTransition
+      position="right"
+      className="bottom-0 left-0 hidden md:inline-block md:absolute"
+    >
+      <Image
+        src="/services.png"
+        width={300}
+        height={300}
+        alt="services-avatar"
+        className="w-[300px]"
+      />
     </MotionTransition>
-  )
+  );
 }
 
-export default AvatarServices
+export default AvatarServices;
--- src/components/circle-image.tsx
+++ src/components/circle-image.tsx
@@ -5,8 +5,8 @@ function CircleImage() {
     <div className="bottom-0 right-0 hidden md:inline-block md:absolute">
       <Image
         src="/circles.png"
-        width="300"
-        height="300"
+        width="200"
+        height="200"
         alt="Circle"
         className="w-full h-full"
       />
