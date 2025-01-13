6aa142a590597076dafc49d43f2b956afcdef7d8
fuerte <leandrofuerte75@gmail.com>
2025-01-13 15:21:28 +0100

    Adding title, description and styling both of them

--- src/components/slider-services.tsx
+++ src/components/slider-services.tsx
@@ -29,12 +29,17 @@ function SliderServices() {
         clickable: true,
       }}
       modules={[Pagination]}
+      loop={true}
       className="h-[280px] md:h-[340px] w-[270px] md:w-[550px]"
     >
       {serviceData.map((service, index) => (
         <SwiperSlide key={index}>
-          <div className="flex px-6 py-8 h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
-            <div>{service.icon}</div>
+          <div className="flex px-[23px] py-8 h-auto md:h-[290px] rounded-lg cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 hover:border-secondary border-2">
+            <div className="mb-4 text-secondary text-4xl">{service.icon}</div>
+            <div>
+              <h3 className="text-lg mb-4 font-bold">{service.title}</h3>
+              <p className="text-sm text-gray-300">{service.description}</p>
+            </div>
           </div>
         </SwiperSlide>
       ))}
