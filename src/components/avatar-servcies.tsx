aaa34c7f8c96b2ef535e0361f8d1f2d6d7186eb3
fuerte <leandrofuerte75@gmail.com>
2025-01-13 09:39:19 +0100

    Creating AvatarServices component

---
+++ src/components/avatar-servcies.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import MotionTransition from "./transition-component"
+
+function AvatarServices() {
+  return (
+    <MotionTransition position="right" className="bottom-0 left-0 hidden md:inline-block md:absolute">
+        <Image src="/services.png" width={300} height={300} alt="services-avatar" className="w-[350px] h-full" />
+    </MotionTransition>
+  )
+}
+
+export default AvatarServices
