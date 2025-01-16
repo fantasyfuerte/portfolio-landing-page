03a6c5c12849abebcc133e2b5aa7c651e215c4bd
fuerte <leandrofuerte75@gmail.com>
2025-01-16 12:51:23 +0100

    Fixing responsive

--- src/app/(routes)/portfolio/page.tsx
+++ src/app/(routes)/portfolio/page.tsx
@@ -16,7 +16,7 @@ function PortfolioPage() {
           Mis ultimos{" "}
           <span className="font-bold text-secondary">trabajos realizados</span>
         </h1>
-        <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4">
+        <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-3 lg:grid-cols-4">
           {dataPortfolio.map((data) => (
             <PortfolioBox data={data} key={data.id} />
           ))}
