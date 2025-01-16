1589573832890aba3ac9c128c677575bbb32a1e0
fuerte <leandrofuerte75@gmail.com>
2025-01-16 11:26:45 +0100

    Creating avatar portfolio

--- src/components/avatar-portfolio.tsx
+++ src/components/avatar-portfolio.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import MotionTransition from "./transition-component";
+import Image from "next/image";
 
 function AvatarPortfolio() {
   return (
-    <div>AvatarPortfolio</div>
-  )
+    <MotionTransition
+      position="bottom"
+      className="bottom-0 left-0 hidden md:inline-block md:absolute"
+    >
+      <Image
+        src="/avatar-works.png"
+        alt="portfolio-image"
+        width={300}
+        height={300}
+      />
+    </MotionTransition>
+  );
 }
 
-export default AvatarPortfolio
+export default AvatarPortfolio;
