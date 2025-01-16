56a8ef5e36429405857444218f37287f7628ff76
fuerte <leandrofuerte75@gmail.com>
2025-01-16 11:39:29 +0100

    Adding and styling title

--- src/app/(routes)/portfolio/page.tsx
+++ src/app/(routes)/portfolio/page.tsx
@@ -9,6 +9,12 @@ function PortfolioPage() {
       <TransitionPage />
       <AvatarPortfolio />
       <CircleImage />
+      <div className="flex flex-col justify-center h-full">
+        <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
+          Mis ultimos{" "}
+          <span className="font-bold text-secondary">trabajos realizados</span>
+        </h1>
+      </div>
     </Container>
   );
 }
