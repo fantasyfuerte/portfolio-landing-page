78c5454d1a0fcff5b975566d9f1d1be36a073e7e
fuerte <leandrofuerte75@gmail.com>
2025-01-16 12:50:36 +0100

    Styling portfolioBox component

--- src/components/portfolio-box.tsx
+++ src/components/portfolio-box.tsx
@@ -24,20 +24,22 @@ function PortfolioBox(props: PortfolioBoxProp) {
         height={200}
         className="w-full md:w-[200px] rounded-2xl h-auto"
       />
-      <div className="flex gap-5 mt-5">
+      <div className="flex gap-5 mt-5 justify-center">
         <a
           href={urlGithub}
           target="_blank"
-          className="p-2 transition duration-150 rounded-lg bg-slate-500 hover:bg-slate-500/80"
+          className="p-2 transition duration-150 rounded-lg bg-slate-500 hover:bg-slate-500/80 flex"
         >
           <GithubIcon />
+          <span className="">Github</span>
         </a>
         <a
           href={urlDemo}
           target="_blank"
-          className="p-2 transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80"
+          className="p-2 transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80 flex"
         >
           <TvMinimalPlay />
+          <span className="ml-[3px]">Demo</span>
         </a>
       </div>
     </div>
