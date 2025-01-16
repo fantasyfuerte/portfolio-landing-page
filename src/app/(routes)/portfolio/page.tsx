875f0b8daab7b70aca020aef07edf8514172f9bc
fuerte <leandrofuerte75@gmail.com>
2025-01-16 12:25:20 +0100

    Adding key to map

--- src/app/(routes)/portfolio/page.tsx
+++ src/app/(routes)/portfolio/page.tsx
@@ -18,7 +18,7 @@ function PortfolioPage() {
         </h1>
         <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4">
           {dataPortfolio.map((data) => (
-            <PortfolioBox key={data.id} />
+            <PortfolioBox data={data} key={data.id} />
           ))}
         </div>
       </div>
