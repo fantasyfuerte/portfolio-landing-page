cf631112cb587977c37e391586ce0540f0e4f252
fuerte <leandrofuerte75@gmail.com>
2025-01-17 07:59:08 +0100

    Adding and styling title

--- src/app/(routes)/testimonials/page.tsx
+++ src/app/(routes)/testimonials/page.tsx
@@ -1,9 +1,19 @@
+import CircleImage from "@/components/circle-image";
 import TransitionPage from "@/components/transition-page";
 
 function TestimonialsPage() {
   return (
     <>
       <TransitionPage />
+      <div className="flex flex-col justify-center h-lvh">
+        <CircleImage />
+        <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
+          Algunos comentarios
+          <span className="block text-secondary font-bold">
+            de nuestros clientes
+          </span>
+        </h1>
+      </div>
     </>
   );
 }
