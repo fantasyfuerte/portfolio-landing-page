372c55941eca8e0fd53eeb451d346e52fbb5c1a9
fuerte <leandrofuerte75@gmail.com>
2025-01-17 07:25:30 +0100

    Styling page

--- src/app/(routes)/portfolio/page.tsx
+++ src/app/(routes)/portfolio/page.tsx
@@ -11,12 +11,12 @@ function PortfolioPage() {
       <TransitionPage />
       <AvatarPortfolio />
       <CircleImage />
-      <div className="flex flex-col justify-center h-full -mt-10">
+      <div className="flex flex-col justify-center h-full">
         <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
           Mis ultimos{" "}
           <span className="font-bold text-secondary">trabajos realizados</span>
         </h1>
-        <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-3 lg:grid-cols-4 pb-28">
           {dataPortfolio.map((data) => (
             <PortfolioBox data={data} key={data.id} />
           ))}
